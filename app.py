9bf1eca503f33f54e805f5f69678be4ed4886997
Unbekannt28 <mika@schiessler.net>
2026-02-25 23:48:33 +0100

    Added SQLite database and database creation sequence

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
 import sqlite3
 
+# connect to database
+con = sqlite3.connect("database.db")
+cur = con.cursor()
+
+# create database, if it was not initialized before
+cur.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, password VARCHAR, games_won INTEGER, games_lost INTEGER, games_played INTEGER);")
+cur.execute("CREATE TABLE IF NOT EXISTS games(id INTEGER PRIMARY KEY AUTOINCREMENT, is_over BOOLEAN, date TIMESTAMP, player_1 INTEGER, player_2 INTEGER, winner INTEGER);")
+cur.execute("CREATE TABLE IF NOT EXISTS moves(id INTEGER PRIMARY KEY AUTOINCREMENT, game INTEGER, turn INTEGER, player INTEGER, position_x INTEGER, position_y INTEGER);")
+
 app = Flask(__name__, static_url_path="/")
 
 @app.route("/")
